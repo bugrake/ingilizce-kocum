d4e42bd9e314ad0ef5243d820e061aaa28216068
bugrake <budevleticin@gmail.com>
2026-01-09 03:03:07 +0300

    app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ API_KEY = st.secrets["GEMINI_KEY"]
 
 try:
     genai.configure(api_key=API_KEY)
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('models/gemini-pro')
     ai_aktif = True
 except:
     ai_aktif = False
